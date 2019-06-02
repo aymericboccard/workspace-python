4d5099fc4288c5631c12581426560e19eb66bcfa
Aymeric Boccard <aymeric.boccard@kbrwadventure.com>
2019-06-02 18:02:39 +0200

    first commit

---
+++ weather.py
@@ -0,0 +1,39 @@
+import requests
+import re
+
+def url_meteo_france(city, zipcode):
+    return 'http://www.meteofrance.com/previsions-meteo-france/'+city.lower()+'/'+zipcode
+
+def get(url):
+    return requests.get(url)
+
+def extract_meteo_france(body):
+    data_to_extract = {
+            "current-temperature": "day-summary-temperature",
+            "sky": "day-summary-label",
+            "UV": "day-summary-uv"
+            }
+    data = {}
+
+    for k in data_to_extract.keys():
+        data[k] = re.search(r"<li class=\""+data_to_extract[k]+"\"> ?(\w+ ?°?\w*)", body).group(1)
+    return data
+
+def extract(source, body):
+    if(source == "meteo_france"):
+        return extract_meteo_france(body)
+    else:
+        return {}
+
+def find(city, zipcode=''):
+    sources = {"meteo_france": url_meteo_france(city, zipcode)}
+    data = {}
+    for k in sources.keys():
+        resp = get(sources[k])
+        if(resp.status_code == 200):
+            data[k] = extract(k, resp.text)
+        else:
+            continue
+
+    return data
+
